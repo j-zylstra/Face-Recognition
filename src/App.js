d34c7520c4959d27c8c064f46f3882f70a25be0e
Joe Zylstra <j-zylstra@hotmail.com>
2023-08-25 12:29:43 +0100

    "cors-a"

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ displayFaceBox = (box) => {
          .then(response => response.json())
          .then(response => {
           if (response) {
-            fetch('https://cors.anywhere.git.heroku.com/evening-lowlands-07472.git/image', {
+            fetch('https://cors-anywhere.git.heroku.com/evening-lowlands-07472.git/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
