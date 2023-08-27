bd7847a9729c08777162abbafe1859eb92be89fd
Joe Zylstra <j-zylstra@hotmail.com>
2023-08-27 10:51:13 +0100

    "new url"

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -21,7 +21,7 @@ class Signin extends React.Component {
   onSubmitSignIn = async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch('https://git.heroku.com/evening-lowlands-07472.git/signin', {
+      const response = await fetch('https://evening-lowlands-07472-04ef7f65244f.herokuapp.com/signin', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
